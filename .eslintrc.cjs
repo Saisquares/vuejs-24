7a441950cefa288a05429414760a54e930f0d598
saisquares <darisettisaikumar@gmail.com>
2025-03-01 18:31:07 +0530

    initial-commit

---
+++ .eslintrc.cjs
@@ -0,0 +1,18 @@
+import '@rushstack/eslint-patch/modern-module-resolution'
+
+export default {
+  root: true,
+  extends: [
+    'plugin:vue/vue3-essential',
+    'eslint:recommended',
+    '@vue/eslint-config-typescript/recommended',
+    'plugin:prettier/recommended',
+    'plugin:vue/vue3-recommended',
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+  },
+  rules: {
+    'vue/multi-word-component-names': 'off',
+  },
+}

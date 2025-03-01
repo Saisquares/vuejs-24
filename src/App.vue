76401fd3b3e48d2d9d26c856fd49f93fff055c84
saisquares <darisettisaikumar@gmail.com>
2025-03-01 19:21:52 +0530

    Lazy Load Routes with Vite’s Dynamic Imports in Vue Router

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,7 @@
 <template>
   <div>
-    <h1>Hello World</h1>
+    <main>
+      <RouterView />
+    </main>
   </div>
 </template>
--- src/router/index.ts
+++ src/router/index.ts
@@ -2,7 +2,23 @@ import { createRouter, createWebHistory } from 'vue-router'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: () => import('@/views/HomeView.vue'),
+    },
+    // {
+    //   path: '/projects',
+    //   name: 'projects',
+    //   component: ProjectsView, // this is a static import causing the page to load slower
+    // },
+    {
+      path: '/projects',
+      name: 'projects',
+      component: () => import('@/views/ProjectsView.vue'), // dynamic  import for lazy loading, causing the page to load faster, bundle size is smaller
+    },
+  ],
 })
 
 export default router
